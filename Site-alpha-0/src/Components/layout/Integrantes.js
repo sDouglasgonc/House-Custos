9b930ab7215db603ecc9afe4be93d1e79b91026f
sDouglasgonc <sdouglas968@gmail.com>
2023-06-07 20:09:12 -0300

    procurar outro caminho
    talvez outra biblioteca

--- Site-alpha-0/src/Components/layout/Integrantes.js
+++ Site-alpha-0/src/Components/layout/Integrantes.js
@@ -1,56 +1,125 @@
-import * as React from "react"
+import React, {useState} from "react"
 import styles from "./Integrantes.module.css"
 import image from "../imgs/homemadulto.png"
+import {wrap} from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+
+
+    const variants = {
+        enter: (direction) => {
+          return {
+            x: direction > 0 ? 1000 : -1000,
+            opacity: 0
+          };
+        },
+        center: {
+          zIndex: 1,
+          x: 0,
+          opacity: 1
+        },
+        exit: (direction) => {
+          return {
+            zIndex: 0,
+            x: direction < 0 ? 1000 : -1000,
+            opacity: 0
+          };
+        }
+      };
+
+const swipeConfidenceThreshold = 1000;
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity;
+};
+
 function Integrantes(){
+    const card2 = [ 
+    <div className={styles.card}>
+    <div className={styles.card_img}>
+    <img src={image} alt="adsa"/>
+    </div>
+    <div className={styles.card_header}>
+    <div className={styles.title}>polenta</div>
+    <div className={styles.sub_title}>Ocupação</div>
+        
+    <p>Resumo sobre a pessoa de acordo</p>
+        
+    </div>
+    </div>
+    ]
+
+const card3 = [ 
+    <div className={styles.card}>
+    <div className={styles.card_img}>
+    <img src={image} alt="adsa"/>
+    </div>
+    <div className={styles.card_header}>
+    <div className={styles.title}>Nome</div>
+    <div className={styles.sub_title}>Ocupação</div>
+        
+    <p>Resumo sobre a pessoa de acordo</p>
+        
+    </div>
+    </div>
+    ]
+
+const [[page, direction], setPage] = useState([0, 0]);
+
+const cardIndex = wrap(0, card2.length, page);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
 
     return(
-<div className={styles.container}>
-<div className={styles.headings}>
-    <h1>blá blá blá</h1>
-</div>
-<div className={styles.slider_wraper}>
-<div className={styles.inner_crousel}>
+        <>
+<AnimatePresence initial={false} custom={direction}>
+<motion.card
+          key={page}
+          src={card2[cardIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
 
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
 <div className={styles.card}>
-<div className={styles.card_img}>
-<img src={image} alt="adsa"/>
-</div>
-<div className={styles.card_header}>
+    <div className={styles.card_img}>
+    <img src={image} alt="adsa"/>
+    </div>
+    <div className={styles.card_header}>
     <div className={styles.title}>Nome</div>
     <div className={styles.sub_title}>Ocupação</div>
-    
+        
     <p>Resumo sobre a pessoa de acordo</p>
+        
+    </div>
+    </div>
     
-</div>
-</div>
+</AnimatePresence>
 
-<div className={styles.card}>
-<div className={styles.card_img}>
-<img src={image} alt="adsa"/>
+ <div className={styles.next} onClick={() => paginate(1)}>
+ {">"}
 </div>
-<div className={styles.card_header}>
-    <div className={styles.title}>Nome</div>
-    <div className={styles.sub_title}>Ocupação</div>
-    
-    <p>Resumo sobre a pessoa de acordo</p>
-    
+<div className={styles.prev} onClick={() => paginate(-1)}>
+ {"<"}
 </div>
-</div>
-
-<div className={styles.card}>
-<div className={styles.card_img}>
-<img src={image} alt="adsa"/>
-</div>
-<div className={styles.card_header}>
-    <div className={styles.title}>Nome</div>
-    <div className={styles.sub_title}>Ocupação</div>
-    <p>Resumo sobre a pessoa</p>
-</div>
-</div>
-
-</div>
-</div>
-</div>
+</>
     )
 }
 export default Integrantes
